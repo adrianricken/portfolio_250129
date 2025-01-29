974db5ce92aace8eb48546dd57d29c7d00e13596
Adrian Ricken <adrianricken@gmail.com>
2025-01-29 16:20:01 +0100

    add skills component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
 import NavBar from "@/components/NavBar";
+import Main from "@/components/Main";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
 
 export default function Home() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <About />
+      <Skills />
+    </>
+  );
 }
