577f2bc94918f8e6b195f08a80a35b0d3fbd32f7
Adrian Ricken <adrianricken@gmail.com>
2025-02-08 19:38:34 +0100

    final version of about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,15 @@ const About = () => {
     <>
       {/* <TransparentHeader /> */}
       <div className="flex justify-center min-h-screen">
-        <div className="w-full max-w-[1680px] min-h-screen grid grid-cols-1 md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr] gap-10 pt-5 md:pt-10">
+        <div className="relative w-full max-w-[1680px] min-h-screen grid grid-cols-1 md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr] pt-10">
           {/* Linker Container */}
-          <div className="relative flex flex-col items-center pl-5 md:pl-10">
+          <div className="relative flex flex-col items-center pl-5 md:pl-8 pr-10">
             <h1 className="text-[var(--text-color1)]">
               Hi, ich bin{" "}
               <span className="text-[var(--highlight-color)]">Adrian</span>
             </h1>
             <h2>Junior Frontend Developer</h2>
-            <div className="w-full flex items-center my-4 justify-between pt-5">
+            <div className="w-full flex items-center my-4 justify-around pt-3">
               <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer ease-in duration-300">
                 <a
                   href="https://www.linkedin.com/in/adrian-ricken/"
@@ -50,19 +50,7 @@ const About = () => {
                 </div>
               </a>
             </div>
-            <div>
-              <Image
-                src={Profile}
-                alt="profile picture"
-                width={320}
-                className="absolute bottom-10 left-23"
-              />
-            </div>
-          </div>
-
-          {/* Rechter Container */}
-          <div className="flex justify-center pt-10 flex-col items-center pr-10">
-            <p className="w-150 relative text-justify leading-relaxed text-xl font-bold text-[var(--text-color1)] lg:text-3xl">
+            <p className="w-100 sm:w-120 md:hidden font-bold text-lg md:text-xl text-justify leading-relaxed pt-5 pl-5 pr-5">
               Design, Struktur und Funktionalität haben mich schon immer
               fasziniert. Mein Architekturstudium hat mir das Denken in
               Systemen, präzise Planung und nutzerzentrierte Gestaltung
@@ -71,6 +59,29 @@ const About = () => {
               ästhetische und funktionale Lösungen zu schaffen.
             </p>
           </div>
+
+          <Image
+            src={Profile}
+            alt="profile picture"
+            width={320}
+            className="absolute bottom-10 left-1/2 md:left-1/5 lg:left-1/6 -translate-x-1/2"
+          />
+          {/* Rechter Container */}
+          <div className="relative flex justify-center pt-10 flex-col items-center md:pr-10">
+            <p className="hidden md:block w-100 md:w-80 lg:w-150  text-justify leading-relaxed text-xl font-bold text-[var(--text-color1)] md:text-2xl lg:text-3xl absolute sm:top-0 sm:relative">
+              Design, Struktur und Funktionalität haben mich schon immer
+              fasziniert. Mein Architekturstudium hat mir das Denken in
+              Systemen, präzise Planung und nutzerzentrierte Gestaltung
+              beigebracht. Mit dem Web Development Bootcamp bei Spiced habe ich
+              meinen Weg in die digitale Welt angetreten, um durchdachte,
+              ästhetische und funktionale Lösungen zu schaffen.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center min-h-screen">
+        <div className="relative w-full max-w-[1680px] min-h-screen grid grid-cols-1 md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr] pt-5 md:pt-10">
+          TEST
         </div>
       </div>
     </>
