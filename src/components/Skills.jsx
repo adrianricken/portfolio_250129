edd345149e1e5b1ceda16ebd2520ccd4b3867fe3
Adrian Ricken <adrianricken@gmail.com>
2025-02-10 00:02:34 +0100

    remove outdated files

--- src/components/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import SkillCard from "./SkillCard";
-import { skills } from "../../data/skillsData";
-
-const Skills = () => {
-  return <div id="skills" className="w-full lg:h-screen p-2"></div>;
-};
-
-export default Skills;
