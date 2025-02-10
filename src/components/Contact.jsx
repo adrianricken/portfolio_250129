5d6bacc1f0ba3ea2dec9c372765245fb4bc0716c
Adrian Ricken <adrianricken@gmail.com>
2025-02-11 00:11:03 +0100

    rearrange repo and deployment links under project image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div id="contact" className="flex justify-center h-1/2">
       <div className="relative w-full max-w-[1680px] grid grid-cols-1 md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr]">
         {/* Linker Container */}
-        <div className="relative hidden md:flex flex-col items-center md:border-r-2 md:border-dotted"></div>
+        <div className="hidden md:flex flex-col flex-grow items-center md:border-r-2 md:border-dotted"></div>
 
         {/* Rechter Container */}
         <div className="relative flex flex-col justify-center items-center w-full p-10">
