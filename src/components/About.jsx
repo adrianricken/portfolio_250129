279d4e72f31ece0d7e20f28a979d16797b2234ca
Adrian Ricken <adrianricken@gmail.com>
2025-02-11 18:33:53 +0100

    final setup

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
     <>
       <div
         id="about"
-        className="flex justify-center md:min-h-screen mt-10 md:mt-0"
+        className="flex justify-center md:min-h-screen pt-10 md:mt-0"
       >
         <div className="relative w-full max-w-[1680px] h-content md:min-h-screen grid grid-cols-1 md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr]">
           {/* Linker Container */}
-          <div className="relative flex flex-col items-center md:px-15 pt-10 md:pt-20 md:border-r-2 md:border-dotted">
+          <div className="relative flex flex-col items-center md:px-15 pt-10 md:border-r-2 md:border-dotted">
             <h1 className="md:text-5xl md:self-start">
               Hi, ich bin{" "}
               <span className="text-[var(--highlight-color)]">Adrian</span>
@@ -41,7 +41,7 @@ const About = () => {
             alt="profile picture"
             width={320}
             height={320} // Setze eine Höhe hinzu
-            className="hidden md:block absolute bottom-10 left-1/2 md:left-1/5 lg:left-1/6 -translate-x-1/2"
+            className="hidden md:block absolute bottom-20 left-1/2 md:left-1/5 lg:left-1/6 -translate-x-1/2"
           />
 
           {/* Rechter Container */}
